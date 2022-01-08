15557bf0aef141560755314d35011ffda13dee14
rhynec <rhynec@pm.me>
2022-01-07 20:28:45 -0600

    Clean-Up
    - Removed pre-compiled objects (except shader).
    - Removed docs.

--- lib/build/realityVP.d
+++
@@ -1,4 +0,0 @@
-realityVP.o: f:/PSDK3v2/sources/Tiny3D/lib/source/realityVP.c \
- f:/PSDK3v2/sources/Tiny3D/lib/source/realityVP.h
-
-f:/PSDK3v2/sources/Tiny3D/lib/source/realityVP.h:
--- lib/build/vshader_text_normal.vcg.d
+++
@@ -1,2 +0,0 @@
-vshader_text_normal.vcg.o: \
- f:/PSDK3v2/sources/Tiny3D/lib/source/vshader_text_normal.vcg.S
